a2e58d46faa0fd101ca1c48af24872ebc87d20fb
mauriciozuniga125 <148015023+mauriciozuniga125@users.noreply.github.com>
2025-10-04 02:51:49 -0500

    Update marcaService.js

--- src/application/marcaService.js
+++ src/application/marcaService.js
@@ -6,6 +6,28 @@ class MarcaService {
   async obtenerMarcas() {
     return await this.marcaRepository.findAll();
   }
+
+  async crearMarca(nombre) {
+    if (!nombre || !/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/.test(nombre)) {
+      throw new Error("El nombre solo puede contener letras");
+    }
+    return await this.marcaRepository.create(nombre);
+  }
+
+  async actualizarMarca(id, nombre) {
+    if (!nombre || !/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/.test(nombre)) {
+      throw new Error("El nombre solo puede contener letras");
+    }
+    return await this.marcaRepository.update(id, nombre);
+  }
+
+  async eliminarMarca(id) {
+    return await this.marcaRepository.delete(id);
+  }
+
+  async obtenerMarcaPorId(id) {
+    return await this.marcaRepository.findById(id);
+  }
 }
 
 module.exports = MarcaService;
